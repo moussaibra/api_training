e01e99c15c707288a1e6a36e4bcdf484c1af8a5b
Moussa Ndiaye <mondiaye@et.esiea.fr>
2021-04-02 17:35:27 +0200

    update 2

--- src/main/java/fr/esiea/ex4A/ApiController.java
+++ src/main/java/fr/esiea/ex4A/ApiController.java
@@ -1,22 +1,29 @@
 package fr.esiea.ex4A;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 class ApiController {
+
+    private final ApiRepository apiRepository;
+
+
+
+
     private final ApiService apiService;
 
-    ApiController(ApiService apiService) {
+    public ApiController(ApiService apiService,ApiRepository apiRepository) {
         this.apiService = apiService;
+        this.apiRepository=apiRepository;
     }
+
+    HashMap<String, String> userValue = new HashMap<String, String>();
     @PostMapping(path="/api/inscription")
     void inscription(@RequestBody UserInfo userInfo) throws InterruptedException{
-        HashMap<String, String> userValue = new HashMap<String, String>();
+
         userValue.put("userCountry", userInfo.userCountry);
         userValue.put("userEmail", userInfo.userEmail);
         userValue.put("userName", userInfo.userName);
@@ -28,14 +35,32 @@ class ApiController {
 
     }
 
+
+
+
+
+
+    UserData sayHello(@RequestParam(name = "name", required = false) String userName, String userCountry , String userTweeter) {
+        final UserData userData;
+
+        if (userName.equals(userValue.get(userName))) {
+            userData = apiRepository.getUserDataFor(userName,userTweeter);
+        }
+        else
+        {
+            userData=null;
+        }
+
+        return userData;
+    }
+
     @GetMapping("/api/matches")
     List<UserData> matches() {
         return List.of(
-            new UserData("titi", "machin"),
-            new UserData("tutu", "bidule")
+            new UserData(userValue.get("userName"), userValue.get("userTweeter")),
+            new UserData(userValue.get("userName"), userValue.get("userTweeter"))
         );
-        }
-
+    };
 
 
 }
--- src/main/java/fr/esiea/ex4A/ApiRepository.java
+++ src/main/java/fr/esiea/ex4A/ApiRepository.java
@@ -10,26 +10,15 @@ import java.util.Random;
 
     @Repository
     class ApiRepository {
-        final List<String> names = List.of(
-            "Jaguabyss",
-            "Coyolax",
-            "Gazena",
-            "Chickig",
-            "Gladiabat",
-            "Gladiafly",
-            "Steeleen",
-            "Marsharak",
-            "Berriot",
-            "Penguine"
-        );
+
         private final Random random = new Random();
 
         /*UserData randomUser() {
             return new UserData(names.get(random.nextInt(names.size())));
         }*/
 
-        /*UserData getUserDataFor(String name) {
-            int letterIndex = name.toLowerCase(Locale.ROOT).charAt(0) - 'a';
-            return new UserData((name + " ").repeat(1 + letterIndex).trim());
-        }*/
+        UserData getUserDataFor(String name,String twitter) {
+
+            return new UserData(name  , twitter );
+        }
 }
